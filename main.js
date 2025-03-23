1ceb6801140a28df9f9ac871ff973d0c518d6ce4
EFairbrother <167487827+EFairbrother@users.noreply.github.com>
2025-03-23 06:55:41 -0400

    added js needs to be deleted

--- main.js
+++ main.js
@@ -1,14 +1,11 @@
-// script.js
-/*document.querySelector('.upload-file').addEventListener('click', () => {
-    alert('Upload file functionality coming soon!');
-  });
-  
-  document.querySelector('.upload-link').addEventListener('click', () => {
-    alert('Upload link functionality coming soon!');
-  });
-  
-  document.querySelector('.help-btn').addEventListener('click', () => {
-    alert('This is the Brain Rottinator. Transform your brain now!');
-  });*/
+document.getElementById("ChangeImageButton").addEventListener("click",function ()){
 
-  
+
+  let img = document.getElementById("help");
+
+  img.src = img.src.includes("")
+
+
+
+
+}
