56afa69e476dce6ddf0e4077ca8da0831106c747
EFairbrother <167487827+EFairbrother@users.noreply.github.com>
2025-03-23 10:06:14 -0400

    Stay Hard

--- main.js
+++ main.js
@@ -1,11 +1,23 @@
-document.getElementById("ChangeImageButton").addEventListener("click",function ()){
+function changeImage(id,newSrc){
 
-
-  let img = document.getElementById("help");
-
-  img.src = img.src.includes("")
-
-
+  document.getElementById(id).src=newSrc;
 
 
 }
+
+function changeBack(id,originalSrc){
+
+  document.getElementById(id)=originalSrc;
+}
+
+document.getElementById("goggins").addEventListener("click",function(){
+
+  changeImage("goggins","/stayhard.jpeg");
+
+})
+
+document.getElementById("/stayhard.jpeg").addEventListener("click",function(){
+
+  changeBack("/stayhard.jpeg","goggins");
+
+})
