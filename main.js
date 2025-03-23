1f19f26027b1449cc9772c7bf2a30ee2faf60eca
Alex Stockford <157730597+LieutLet@users.noreply.github.com>
2025-03-23 02:07:10 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,14 @@
+// script.js
+/*document.querySelector('.upload-file').addEventListener('click', () => {
+    alert('Upload file functionality coming soon!');
+  });
+  
+  document.querySelector('.upload-link').addEventListener('click', () => {
+    alert('Upload link functionality coming soon!');
+  });
+  
+  document.querySelector('.help-btn').addEventListener('click', () => {
+    alert('This is the Brain Rottinator. Transform your brain now!');
+  });*/
+
+  
